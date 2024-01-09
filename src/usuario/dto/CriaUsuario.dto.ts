7709ff226c4d9f3fbd1caa57a40d2d2553203f37
Henrique Barucco <henrique_barucco@hotmail.com>
2024-01-09 15:45:10 -0300

    feat: email unico annotation

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,10 +1,12 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { EmailUnico } from '../validacao/email-unico.validator';
 
 export class CriaUsuarioDTO {
     @IsNotEmpty({ message: 'Nome é obrigatório' })
     nome: string;
 
     @IsEmail({}, { message: 'E-mail inválido' })
+    @EmailUnico({ message: 'E-mail já cadastrado' })
     email: string;
 
     @MinLength(6, { message: 'Senha deve ter no mínimo 6 caracteres' })
